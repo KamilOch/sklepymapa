d7a041920496c46a24aa02d5240c9bfabf0404e9
Kamil Ochnik <ilmak@o2.pl>
2019-11-21 09:58:07 +0100

    first commit

---
+++ src/main/java/com/kamil/sklepymapa/sklepymapa/SklepymapaApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.sklepymapa.sklepymapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepymapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepymapaApplication.class, args);
+	}
+
+}
