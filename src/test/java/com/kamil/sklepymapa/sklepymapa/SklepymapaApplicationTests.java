7d353486257241e8888a38fb3d10e4d25f9a5d81
Kamil Ochnik <ilmak@o2.pl>
2019-11-21 10:02:07 +0100

    first commit

---
+++ src/test/java/com/kamil/sklepymapa/sklepymapa/SklepymapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.sklepymapa.sklepymapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklepymapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
