c1c18b3efbb544e006f78a867ee3b2b59935e90e
Kamil Ochnik <ilmak@o2.pl>
2019-12-04 15:10:52 +0100

    added functionality ( adding shop and displaying shops list ) in work

--- src/main/java/com/kamil/sklepymapa/sklepymapa/ProgramController.java
+++ src/main/java/com/kamil/sklepymapa/sklepymapa/ProgramController.java
@@ -1,24 +1,45 @@
 package com.kamil.sklepymapa.sklepymapa;
 
+import com.kamil.sklepymapa.sklepymapa.shop.dto.ShopDTO;
+import com.kamil.sklepymapa.sklepymapa.shop.service.ShopService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProgramController {
 
-    @GetMapping("/admin")
-    public String admin(
-            Model model
-    ) {
+    private final ShopService shopService;
+    private final ShopDTO shopDTO;
 
+    public ProgramController(ShopService shopService, ShopDTO shopDTO) {
+        this.shopService = shopService;
+        this.shopDTO = shopDTO;
+    }
+
+
+    @RequestMapping("/dodajSklep")
+    public String dodajSklep(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "streetName", required = false) String streetName,
+            @RequestParam(value = "streetNumber", required = false) Integer streetNumber
+    ) {
+        if (name != null && streetName != null && streetNumber != null) {
+            ShopDTO newShop = ShopDTO.builder().name(name).streetName(streetName).streetNumber(streetNumber).build();
+            System.out.println(newShop.toString());
+            shopService.addShop(newShop);
+        }
         return "admin";
     }
 
-    @GetMapping("/user")
-    public String user(
+
+    @GetMapping("/shops")
+    public String shopList(
             Model model
     ) {
+        model.addAttribute("sklep", shopService.getAllShops());
         return "user";
     }
 
